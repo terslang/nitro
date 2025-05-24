816c622fddb29ce4746a9042cdff65baff9c60c6
Akash Doppalapudi <akash@ters.dev>
2025-05-24 16:42:45 +0530

    added a ceil div method

--- pkg/helpers/helpers.go
+++ pkg/helpers/helpers.go
@@ -2,12 +2,21 @@ package helpers
 
 import "fmt"
 
-func GetPartialContentSize(contentLength uint64, parallel uint8) (uint64, error) {
-	if parallel == 0 {
-		return 0, fmt.Errorf("Parallel: 0, Division by zero")
+func ceilDiv(a uint64, b uint64) (uint64, error) {
+	if b == 0 {
+		return 0, fmt.Errorf("Denominator: 0, Division by zero")
 	}
 
-	return (contentLength + uint64(parallel) - 1) / uint64(parallel), nil
+	return (a + b - 1) / b, nil
+}
+
+func GetPartialContentSize(contentLength uint64, parallel uint8) (uint64, error) {
+	partialContentSize, err := ceilDiv(contentLength, uint64(parallel))
+	if err != nil {
+		return 0, fmt.Errorf("Error calculating partial content size: %w", err)
+	}
+
+	return partialContentSize, nil
 }
 
 func CalculateFromAndToBytes(contentLength uint64, partialContentSize uint64, partNo uint8) (uint64, uint64) {
